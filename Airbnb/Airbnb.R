40cd75ba9ed718f39dbb64b260827049bdf4d6ce
Jan Červenka <jan.cervenka@yahoo.com>
2017-09-20 01:46:42 +0200

    Mean coords

---
+++ Airbnb/Airbnb.R
@@ -0,0 +1,8 @@
+data <- Airbnb_Texas_Rentals
+
+cities <- names(tail(sort(table(data[, 'city'])), 5))
+
+subdata <- data[data$city %in% cities,][, c("city", "longitude", "latitude")] #comma means all rows
+
+agg <- aggregate(latitude ~ city, subdata, mean)
+agg[, 'longitude'] <- aggregate(longitude ~ city, subdata, mean)[, 'longitude']
