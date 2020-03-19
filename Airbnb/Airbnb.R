96d74c0500a0f7575b8be71eb015d99f06d152e1
Jan <jan.cervenka@yahoo.com>
2020-03-20 00:19:57 +0100

    breast cancer classifier, cleanup

--- Airbnb/Airbnb.R
+++
@@ -1,8 +0,0 @@
-data <- Airbnb_Texas_Rentals
-
-cities <- names(tail(sort(table(data[, 'city'])), 5))
-
-subdata <- data[data$city %in% cities,][, c("city", "longitude", "latitude")] # comma means all rows
-
-mean_coords <- aggregate(latitude ~ city, subdata, mean)
-mean_coords[, 'longitude'] <- aggregate(longitude ~ city, subdata, mean)[, 'longitude']
